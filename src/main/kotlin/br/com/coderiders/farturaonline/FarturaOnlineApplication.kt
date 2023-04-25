8b851b7e2e6de65d227d39ac6e5307a26b548009
Thiago Santana <thiago91098@gmail.com>
2023-04-24 21:43:06 -0300

    first commit

---
+++ src/main/kotlin/br/com/coderiders/farturaonline/FarturaOnlineApplication.kt
@@ -0,0 +1,11 @@
+package br.com.coderiders.farturaonline
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FarturaOnlineApplication
+
+fun main(args: Array<String>) {
+	runApplication<FarturaOnlineApplication>(*args)
+}
---
+++ src/test/kotlin/br/com/coderiders/farturaonline/FarturaOnlineApplicationTests.kt
@@ -0,0 +1,13 @@
+package br.com.coderiders.farturaonline
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class FarturaOnlineApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
